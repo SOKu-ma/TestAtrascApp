21dcc684c2408888e2b34786c8cec18ea9bc9cc1
SOKu-ma <sokumaeng2020@gmail.com>
2020-10-22 17:06:52 +0900

    Initial Commit

---
+++ TestAtrascApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAtrascApp
+//
+//  Created by 太田修平 on 2020/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
