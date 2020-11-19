76f10da40e9750a6b3fca731c8bc4aeb288953df
SOKu-ma <sokumaeng2020@gmail.com>
2020-11-19 23:00:38 +0900

    Mod LoginView Storyboard etc 2020/11/19

--- TestAtrascApp/Controller/LoginViewController.swift
+++ TestAtrascApp/Controller/LoginViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // ユーザID
+    @IBOutlet weak var contViewUserID: UIView!
+    @IBOutlet weak var imageUserID: UIImageView!
+    @IBOutlet weak var textUserID: UITextField!
+    
+    
+    // パスワード
+    @IBOutlet weak var contViewPW: UIView!
+    @IBOutlet weak var imagePW: UIImageView!
+    @IBOutlet weak var textPW: UITextField!
+    
+    // ログインボタン
     @IBOutlet weak var btnLogin: UIButton!
     
     override func viewDidLoad() {
@@ -16,13 +28,8 @@ class LoginViewController: UIViewController {
         
         // ボタンの整形
         // ログイン
-//        btnLogin.layer.borderColor = UIColor.gray.cgColor
         btnLogin.layer.borderWidth = 1
         btnLogin.layer.cornerRadius = 20
-//        btnLogin.layer.shadowOpacity = 0.7
-//        btnLogin.layer.shadowRadius = 3
-//        btnLogin.layer.shadowColor = UIColor.black.cgColor
-//        btnLogin.layer.shadowOffset = CGSize(width: 5, height: 5)
         btnLogin.layer.borderColor = UIColor.systemBlue.cgColor
         btnLogin.layer.backgroundColor = UIColor.systemBlue.cgColor
         btnLogin.setTitleColor(UIColor.white, for: .normal)
---
+++ TestAtrascApp/View/ClubInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClubInfoTableViewCell.swift
+//  TestAtrascApp
+//
+//  Created by 太田修平 on 2020/10/26.
+//
+
+import UIKit
+
+class ClubInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+    
+}
